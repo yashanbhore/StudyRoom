2e1d5cf5a6b62e7545439ccde7d9ac96d346dcd8
Yash <yanbhore@gmail.com>
2023-11-10 19:26:37 +0530

    Working on Login

--- base/views.py
+++ base/views.py
@@ -1,14 +1,40 @@
 from arrow import get
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
-from .models import Room,Topic
+from .models import Room,Topic,User
 from .forms import RoomForm
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
+from django.contrib import messages
+
+def loginPage(request):
+    
+
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+        
+        try: 
+            user = User.objects.get(email=email )
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, email=email, password=password)
+        
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
-    rooms=Room.objects.all()
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+    rooms=Room.objects.filter(
+            Q(topic__name__icontains=q) |
+            Q(name__icontains=q) |
+            Q(description__icontains=q)
+            )
+    room_count = rooms.count()
     topics=Topic.objects.all()
-    context={'rooms':rooms, 'topics':topics}
+    context={'rooms':rooms, 'topics':topics,'room_count': room_count}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
