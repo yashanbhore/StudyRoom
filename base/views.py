e357dacd4e8e4e8e217ce3144e936baf49308ece
Yash <yanbhore@gmail.com>
2023-11-10 15:52:31 +0530

    Edit and Delete Functionality

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from arrow import get
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
 
 
 def home(request):
     rooms=Room.objects.all()
-    return render(request,'base/home.html',{'rooms':rooms})
+    topics=Topic.objects.all()
+    context={'rooms':rooms, 'topics':topics}
+    return render(request,'base/home.html',context)
 
 def room(request,pk):
     rooms=Room.objects.get(id=pk)
@@ -22,3 +24,31 @@ def createRoom(request):
            return redirect('home')
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here.') 
+    
+    if request.method=='POST':
+        form=RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context={'form': form}
+    return render(request,'base/room_form.html',context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!!') 
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
